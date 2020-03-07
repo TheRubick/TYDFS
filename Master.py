c6e63b94b4e928d341f19e9246df5a403e095478
therubick <mohamed_ayman_07@hotmail.com>
2020-03-07 13:11:02 +0200

    Watchdogs process version 1.1

--- Master.py
+++ Master.py
@@ -3,17 +3,18 @@ import zmq
 import pandas as pd
 import sys
 import time
-
+import numpy as np
 '''
 Configuration phase of the lookup table
 '''
-data = {
-    'status' : [],
-    'ipv4' : [],
-    'port' : [],
-    'TTL' : [] # time to live i.e. after period of time if the data keeper didn't respond this TTL would be changed
-    #'file' : []
-}
+#intiating the shared memory
+status = multiprocessing.Manager().list()
+ipv4 = multiprocessing.Manager().list()
+port = multiprocessing.Manager().list()
+filePath = multiprocessing.Manager().list()
+
+#intiating the lock for the critical section
+lutLock = multiprocessing.Lock()
 
 dkNum = int(sys.argv[1])
     #making M indices for the data keepers
@@ -21,53 +22,67 @@ dkNum = int(sys.argv[1])
 
     #for loop to intialize the data in the look up table
 for i in range(dkNum):
-    data['status'].append("alive")
-    data['ipv4'].append("tcp://127.0.0.1:")
-    data['port'].append("5556")
-    data['TTL'].append(0)
-
-lookUpTable = pd.DataFrame(data=data)
-print(lookUpTable)
-print(type(lookUpTable))
+    status.append("alive")
+    ipv4.append("tcp://127.0.0.1:")
+    port.append("5556")
+    filePath.append("--") # no need to fill the dkNum first rows of the lookup table as there is no files yet 
 
 
-def subPubTracker():
+def watchDogFunc(status,lutLock):
 
-    #define variable counter to make checks on the data keepers who is alive and who is dead
-    counter = 0
     #defining the port of the pub sub connection
     port = "5556"
 
     #intiating context
     context = zmq.Context()
-    sockt = context.socket(zmq.SUB)
-    sockt.bind("tcp://127.0.0.1:%s" % port)
-    sockt.subscribe(topic="") # topic is empty string so the master accepts any string
+    socket = context.socket(zmq.SUB)
+    socket.bind("tcp://127.0.0.1:%s" % port)
+    socket.subscribe(topic="") # topic is empty string so the master accepts any string
 
+    #intiate whoSent array to be used on making checks on the alive data keepers
+    whoSent = np.zeros((dkNum)) 
+    
     while True:
+
+        start = time.monotonic()
+        #check the alive
+        for i in range(dkNum):
+            num = socket.recv_string()
+            print("dkNUM # "+num)
+            whoSent[int(num)] = 1
+            lutLock.acquire()
+            status[i] = "alive"
+            lutLock.release()
         
-        '''
-        to be modified on making lookup table but it is a temp logic
-        '''
+        #check the dead
+        for i in range(dkNum):
+            if(whoSent[i] == 0):
+                print("dkNUM # "+str(i)+" didn't send")
+                lutLock.acquire()
+                status[i] = "dead"
+                lutLock.release()
         
-        dkNum = sockt.recv_string() # data keeper number
-        #printing the data keeper number and check if it's not alive or not
-        if(int(dkNum) == counter):
-            print("Data Keeper # "+dkNum+" is a live")
-        else:
-            print("Data Keeper # "+dkNum+" isn't a live i.e. it is dead :( ")
-        # delay for 1 second
-        counter += 1
-        counter %= dkNum
-        time.sleep(1)
+        #reset the whoSent list 
+        whoSent = np.zeros(dkNum)
+        
+        end = time.monotonic()
+        print("time taken = "+str(end - start))
+        #wait for the remaing of the 1 second
+        if((end-start) <= 1):
+            time.sleep(1-(end-start))
+        print("----------------------------------------------------------------")
+        
+            
+    
 
-    '''
-    @TODO
-    in download phase , store the file name in lower case , use in operator to search in the file list
-    '''
+'''
+@TODO
+in download phase , store the file name in lower case , use in operator to search in the file list
+'''
 
-subPubProcess = multiprocessing.Process(target=subPubTracker)
+#watch Dog process is used to keep tracking the alive messages from the data keepers
+watchDog = multiprocessing.Process(target=watchDogFunc,args=(status,lutLock))
 
-subPubProcess.start()
+watchDog.start()
 
-subPubProcess.join()
+watchDog.join()
