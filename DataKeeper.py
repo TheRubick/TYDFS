730db092ab941af2b6150506e8f4983b99bf4bc7
therubick <mohamed_ayman_07@hotmail.com>
2020-03-12 11:26:47 +0200

    real data keeper :D sry for this sh**ty fault

--- DataKeeper.py
+++ DataKeeper.py
@@ -4,124 +4,28 @@ import pandas as pd
 import sys
 import time
 import numpy as np
-'''
-Configuration phase of the lookup table
-'''
-#intiating the data of the look up table
-dataLut = {
-    'status'  : [], # status = alive or not alive
-    'dkID'  : [], # data keeper ID = ipaddress:port
-    'fileName'  : [], # file Name
-    'filePath'  : [], # file Path
-    'userID' : []
 
-}
+num = int(sys.argv[1])
 
-#intiating the lock for the critical section
-lutLock = multiprocessing.Lock()
-
-#intialize the dkNUm with the number of data keepers
-dkNum = int(sys.argv[1])
-
-#for loop to intialize the data in the look up table
-for i in range(dkNum):
-    dataLut['status'].append("alive")
-    dataLut['dkID'].append("tcp://127.0.0.1:6000")
-    dataLut['fileName'].append("--")
-    dataLut['filePath'].append("--")
-    dataLut['userID'].append("--") 
-
-def watchDogFunc(sharedLUT):
+def aliveSender():
 
     #defining the port of the pub sub connection
     port = "6000"
 
     #intiating context
     context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.bind("tcp://127.0.0.1:%s" % port)
-    socket.subscribe(topic="") # topic is empty string so the master accepts any string
-
-    #intiate chance to live array to be used on making checks on the alive data keepers , maximum number of chance is 2
-    CTL = np.zeros((dkNum)) 
-    #intialize the set the would contain all the numbers of the data keepers
-    dkSet = set()
-    for i in range(dkNum):
-        dkSet.add(i)
-    
-    #intialize the alive data keepers set
-    aliveSet = set()
-    #intialize the flag to intiate the start time "new period"
-    startPeriod = True
-    #make the start time variable
-    global startTime
-    #intialize temp dictionary to hold the new data frame
-    global tempdf
+    socket = context.socket(zmq.PUB)
+    socket.connect("tcp://127.0.0.1:%s" % port)
     
     while True:
-
-        if(startPeriod):
-            #make the flag of start period with false
-            startPeriod = False
-            #intialize the startTime
-            startTime = time.monotonic()
-            #intialize temp dictionary to hold the new data frame
-            tempdf = sharedLUT.df
-
-        #recieve the num of the data keeper
-        num = socket.recv_string()
-        tempdf[int(num)] = "alive"
-        #in case if the data keeper was dead then waked up again
-        CTL[int(num)] = 0
-        #add the num of this data keeper to the aliveSet
-        aliveSet.add(int(num))
-
-        #get the current time
-        currentTime = time.monotonic()
-        #if the time between the start and current time is greater than 0.5 then check on the alive/dead data keepers
-        if(currentTime - startTime > 0.5):
-            #intialize the notAlive set
-            notAlive = dkSet.difference(aliveSet)
-            #loop on this set
-            while(len(notAlive) != 0):
-                deadDk = notAlive.pop()
-                CTL[deadDk] = CTL[deadDk] + 1
-                #if the chance of the keeper = 2 means it didn't send to the master after 2 seconds , so it will be dead
-                if(CTL[deadDk] == 2): 
-                    tempdf[deadDk] = "dead"
-
-            #make the flag of start period with true
-            startPeriod = True
-            #change the LUT shared between the processes
-            lutLock.acquire()
-            sharedLUT.df = tempdf
-            lutLock.release()
-
-            #reset the aliveSet
-            aliveSet = set()
-
-            #end time would be used to sleep the remaining of the 1 second i.e. sleep remaining of the period
-            endTime = time.monotonic()        
-            print(endTime-startTime)
-            #wait for the remaing of the 1 second
-            if((endTime-startTime) <= 1):
-                time.sleep(1-(endTime-startTime))
-            
-            print("----------------------------------------------------------------")
-            
+        socket.send_string(str(num))
+        time.sleep(1)    
             
     
 
-'''
-@TODO
-in download phase , store the file name in lower case , use in operator to search in the file list
-'''
-#make the lookUpTable data frame then assign it to the shared Memory "sharedLUT" 
-sharedLUT = multiprocessing.Manager().Namespace()
-sharedLUT.df = pd.DataFrame(dataLut)
 #watch Dog process is used to keep tracking the alive messages from the data keepers
-watchDog = multiprocessing.Process(target=watchDogFunc,args=(sharedLUT,))
+aliveProcess = multiprocessing.Process(target=aliveSender,)
 
-watchDog.start()
+aliveProcess.start()
 
-watchDog.join()
+aliveProcess.join()
